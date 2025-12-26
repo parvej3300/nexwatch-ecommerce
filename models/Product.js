3defd8068b4af75bc5145ed6e29f3f89c3e0f6af
parvej3300 <67873053+parvej3300@users.noreply.github.com>
2025-12-26 17:14:17 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,16 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-const ProductSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, default: '' },
-  price: { type: Number, default: 0 },
-  image: { type: String, default: '' },
-  inStock: { type: Boolean, default: true }
-}, { timestamps: true });
+const ProductSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, default: '' },
+    price: { type: Number, default: 0 },
+    image: { type: String, default: '' },
+    inStock: { type: Boolean, default: true }
+  },
+  { timestamps: true }
+);
 
 const Product = mongoose.model('Product', ProductSchema);
-export default Product;
+
+module.exports = Product;
